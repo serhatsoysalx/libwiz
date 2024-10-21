82f93f84ae1cae0f1ac1ab703b7989eb3b922089
Serhat Soysal <serhatt.soysal@gmail.com>
2024-10-21 05:18:52 +0300

    First commit

---
+++ src/main/java/com/libwiz/libwiz/LibwizApplication.java
@@ -0,0 +1,13 @@
+package com.libwiz.libwiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibwizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibwizApplication.class, args);
+	}
+
+}
